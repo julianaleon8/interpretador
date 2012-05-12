fa5c73f60d70164ed3934ea40e1b9f4d6d88f776
juliana <juli@Mercury.(none)>
2012-05-11 23:52:08 -0430

    hola

--- LexAsgard.py
+++ LexAsgard.py
@@ -3,14 +3,13 @@
 
 import lex
 import sys
-
 # lista de tokens 
 tokens = (
 		'TkComa','TkPuntoYComa','TkParAbre','TkParCierra','TkLienzo','TkIdent','TkNum','TkSuma','TkResta','TkMult',
 		'TkDiv','TkMod','TkConjuncion','TkDisyuncion','TkNegacion','TkMenor','TkMenorIgual','TkMayor','TkMayorIgual',
 		'TkIgual','TkDesIgual','TkHorConcat','TkVerConcat','TkRot','TkTras','TkAsignacion',
 		)
-	  
+
 # lista de tokens reservados
 reserved = {
 		'using' : 'TkUsing', 'of type' : 'TkOfType', 'integer' : 'TkInteger', 'boolean' : 'TkBoolean', 'canvas' : 'TkCanvas',
@@ -18,19 +17,23 @@ reserved = {
 		'if' : 'TkIf', 'else' : 'TkElse', 'done' : 'TkDone', 'end' : 'TkEnd', 'then' : 'TkThen', 'read' : 'TkRead',
 		'print' : 'TkPrint', 'true' : 'TkTrue', 'false' : 'TkFalse'
 		}
-	      
+
 precedence= (
 	('left','TkLienzo','TkDiv'),
 	('left','TkLienzo','TkMenor'))
 
 # valores a ignorar
 # los valores de ^ y $ fuera de los corchetes significa que es el inicio de linea y el final de linea respectivamente
-t_ignore = ' \t^{[-a-zA-Z0-9]*}$ '
+
+def t_comment(t):
+	r'^[{][}]$'
+	return None
 
 # para saber el numeros de lineas
 def t_newline(t):
 	r'\n'
 	t.lexer.lineno += 1
+	lista.append('\n')
 
 # definicion de expresiones
 def t_TkNum(t):
@@ -39,25 +42,45 @@ def t_TkNum(t):
 		t.value = int(t.value)
 	except ValueError:
 		print 'Error de decimal'
-	print '{0}({1})'.format(t.type,t.value)
+	lista.append('{0}({1}) '.format(t.type,t.value))
 
 def t_TkIdent(t):
 	r'(([a-zA-Z]([a-zA-Z0-9]*))|(of type))'
         t.type = reserved.get(t.value,'TkIdent')
         if (t.type == reserved.get(t.value,'reserved')):
-	  print t.type
+	  lista.append(t.type+" ")
 	else:
-	  print '{0}("{1}")'.format(t.type,t.value)
+	  lista.append('{0}("{1}") '.format(t.type,t.value))
 
 def t_TkLienzo(t):
 	r'<([/]|[\\]|[\-]|[_]|[ ]|empty)>' # fuck, arreglar esto
-	
+
 #	t.type = tokens.get(t.value,'TkLienzo')
-	print '{0}("{1}")'.format(t.type,t.value[1: len(t.value) - 1])
+	lista.append('{0}("{1}") '.format(t.type,t.value[1: len(t.value) - 1]) )
+
+def find_column(input,token):
+    i = token.lexpos
+    t = 0
+    k = 0   ##inicializo k en 0
+    while i > 0:
+        if input[i] == '\n': break
+        if input[i] == '\t': ##controlar si el caracter es un tab (expresion regular '\t')
+            k +=1            ##si es asi tengo un contador para la cantidad de tabs
+        i -=1                ##resta la cantidad de caracteres (no se considera tab como 8 caracteres, sino como uno solo) desde la columna relativa (la que maneja python) del token hasta el principio de la linea del token
+    if (token.lineno == 1):
+        k +=1
+        t = 0
+    else:
+        t = 1
+    column = ((((token.lexpos - i) + (k*8)) - k) - t) +1      ##formula que calcula las columnas ok
+    return column
+
+t_ignore = ' \t'
 
 def t_error(t):
-	print ("Error: Caracter inesperado %s en la fila %d columna " % (t.value[0],t.lineno))
+	print ("Error: Caracter inesperado %s en la fila %d columna %s " % (t.value[0],t.lineno,find_column(archi,t)))
 	lexer.skip(1)
+	#count = count + 1
 
 # valores de los tokens simples
 
@@ -91,10 +114,17 @@ lexer=lex.lex(debug=0)
 # abrir archivo
 f = open(sys.argv[1],"r")
 # leer archivo
-lexer.input(f.read())
+archi = f.read()
+lexer.input(archi)
+f.close()
+count = 0
+lista = ['']
 while True:
     tok = lexer.token()
-    if not tok: break      # No more input
-    
-   # if tok ==  
-    print tok.type
+    if not tok: 
+	break      # No more input
+    else: 
+	lista.append(tok.type+" ")
+if (count == 0):
+	for i in range (0,len(lista)):
+		sys.stdout.write(lista[i])
